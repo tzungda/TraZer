80f038aa33ccb172e636f1beea615eed5f9e7dd1
tzungda <31838821+tzungda@users.noreply.github.com>
2018-12-30 23:43:00 -0800

    working on texture

--- TraZer/TraZer.cpp
+++ TraZer/TraZer.cpp
@@ -616,7 +616,7 @@ void My_Keyboard(unsigned char key, int x, int y)
 		{
 			tzWorld w;
 			w.build();
-			w.setOutputPath( "C:\\Users\\User\\Desktop\\TraZer\\TraZer\\testImages\\Horse2K_01.png" );
+			w.setOutputPath( "C:\\Users\\User\\Desktop\\TraZer\\TraZer\\testImages\\texture_test.png" );
 			w.renderScene();
 			int a = 0;
 			a = 1;
