16480130038761b0b8267e02329208e6f748d7c0
tzungda <31838821+tzungda@users.noreply.github.com>
2019-01-12 22:47:16 -0800

    working on camera

--- TraZer/Include/tzCoreCamera.h
+++ TraZer/Include/tzCoreCamera.h
@@ -21,50 +21,33 @@ public:
 	tzCoreCamera( );
 	virtual ~tzCoreCamera();
 
-	typedef struct {
-		int vertex_index;
-		int normal_index;
-		int texcoord_index;
-	} index;
 	
+
 private: // member data
-	vector<tzPoint3D> 			mVertices;				// mesh vertices 
-	vector<tzNormal>			mVertexNormals;			// average normal at each vertex;
-	vector<float>				mU;						// u texture coordinate at each vertex
-	vector<float>				mV;						// v texture coordinate at each vertex
-	vector<vector<int> > 		mVertexFaces;			// the triangles shared by each vertex
-	int 						mNumVertices; 			// number of vertices
-	int 						mNumTriangles; 			// number of triangles
-	vector<index>				mIndices;				// indices of the vertices
-	vector<float>				mPositions;				// 
-	vector<vector<int>>			mFaceVertices;
+	tzVector3D	mFace;
+	tzVector3D	mUp;
+	tzVector3D	mRight;
+
+	float		mAspect;
+	float		mFov;
+	float		mNearPlane;
+	float		mFarPlane;
 
 public: // 
 
-	// set data interfaces
-	void							setVertices( const vector< tzPoint3D > &vertices);
-	void							setVertices(const vector< float > &positions);
-	void							setVertexNormals(const vector< tzNormal > &normals);
-	void							setUs(const vector< float > &us);
-	void							setVs(const vector< float > &vs);
-	void							setVertexFaces( const vector<vector<int> > vertexFaces);
-	void							setNumVertices( int numVertices );
-	void							setNumTriangles(int numVertices);
-	void							setIndices( const vector< index > &indices );
-	void							setFaceVertices( const vector<vector<int>> &faceVertices );
+	tzVector3D	faceDir() const;
+	tzVector3D	upDir() const;
+	tzVector3D	rightDir() const;
+	float		aspect( ) const;
+	float		fov( ) const;
+	float		nearPlane( ) const;
+	float		farPlane() const;
+	void		setAspect( float aspect );
+	void		setFov( float fov);
+	void		setNearPlane( float nearPlane);
+	void		setFarPlane(float farPlane);
+	tzMatrix	perspectiveProjection( ) const;
 	
-	
-	// get data interfaces
-	const vector< float >&			floatPositions( ) const;
-	const vector< tzPoint3D >&		vertices( ) const;
-	const vector< tzNormal >&		vertexNormals( ) const;
-	const vector< float >&			us( ) const;
-	const vector< float >&			vs( ) const;
-	const vector<vector<int> >		vertexFaces( ) const;
-	int								numVertices( ) const;
-	int								numTriangles( ) const;
-	const vector< index >&			indices( ) const;
-	const vector<vector<int>>&		faceVertices() const;
 };
 
 #endif
