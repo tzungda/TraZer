53f76aed3d289c93fe97b54fb1274692322e7df3
tzungda <31838821+tzungda@users.noreply.github.com>
2019-01-13 22:14:42 -0800

    working on glCamera

--- TraZer/glView/include/tzGLCamera.h
+++ TraZer/glView/include/tzGLCamera.h
@@ -11,11 +11,7 @@
 	#include <unistd.h>
 #endif
 
-#define GLM_SWIZZLE
-#include "GLM/glm/glm.hpp"
-#include "GLM/glm/gtc/matrix_transform.hpp"
-#include "GLM/glm/gtc/type_ptr.hpp"
-#include "GLM/glm/gtx/rotate_vector.hpp"
+
 
 #include <cstdio>
 #include <cstring>
@@ -24,26 +20,15 @@
 #include <algorithm>
 
 #include "../Include/tzCoreTransform.h"
+#include "../include/tzCoreCamera.h"
+#include "tzPoint2D.h"
 //#include "tzMatrix.h"
 //#include "tzVector3D.h"
 
 class tzNormal;
 
-/**
- * @brief The tzGLCamera class
- * The tzGLCamera class provides viewing manipulation related functionalities.
- *
- * To use the tzGLCamera class, call mousePressEvent(),
- * mouseReleaseEvent(), mouseMoveEvent(), wheelEvent() in your event handlers
- * with the event objects.
- *
- * The viewing manipulation will be done for you in the class. When you are ready
- * to render something, call GetModelMatrix(), GetViewMatrix(), GetProjectionMatrix()
- * and their composite versions to get the MVP matrices which ecode current viewing
- * properties.
- */
 
-class tzGLCamera: public tzCoreTransform
+class tzGLCamera
 {
 public:
     tzGLCamera();
@@ -54,96 +39,41 @@ public:
     void mouseMoveEvent(int x,int y);
 	void keyEvents(unsigned char key);
     void wheelEvent(float direction);
-	void Translate(glm::vec3 vec);
 
-	glm::mat4 GetModelMatrix();
-	glm::mat4 GetViewMatrix();
-	glm::mat4 GetProjectionMatrix(float aspect);
-	glm::mat4 GetViewProjectionMatrix(float aspect);
-	glm::mat4 GetModelViewProjectionMatrix(float aspect);
-	glm::vec3 GetEyePosition() {return eyePosition;}
-	glm::vec3 GetViewVector() {return viewVector;}
+	tzMatrix viewMatrix();
+	tzMatrix projectionMatrix(float aspect);
 
-	glm::vec3 GetWorldEyePosition();
-	glm::vec3 GetWorldViewVector();
 
-    bool IsOrthoProjection() { return ortho; }
+    bool IsOrthoProjection() { return mIsOrtho; }
 
-	void SetCamera(glm::vec3 cameraPos, glm::vec3 focusPos);
-//    void SetRotation(float theta, float phi);
-//    void SetRotation(float x, float y, float z);
-	void SetWindowSize(int width, int height);
+	void setWindowSize(int width, int height);
 
-    bool ToggleOrtho() { return ortho = !ortho; }
-    void Zoom(float distance);
-    void Reset();
+    bool ToggleOrtho() { return mIsOrtho = !mIsOrtho; }
+    void zoom(float distance);
+    void reset();
 
-	//
-	tzVector3D	faceDir( ) const;
-	tzVector3D	upDir() const;
-	tzVector3D	rightDir() const;
+	void		setCamPosition(const tzVector3D& position);
 
-	//
-	/*
-	tzMatrix	rotateMatrixAlongVector( float angle, const tzVector3D &v );
-	tzMatrix	roatateX( float deltaAngle );
-	tzMatrix	roatateY( float deltaAngle );
-	tzMatrix	roatateZ( float deltaAngle );
-	tzMatrix	updateTransformMatrix( );
-	void		setPosition( const tzVector3D& position );
-	*/
-	virtual tzVector3D	move( float deltaX, float deltaY, float deltaZ );
-	//
-	//tzMatrix invertedTransformMatrix( ) const;
 	
 
 private:
-    bool ortho = false;
-    float zoom = 1.0f;
-	float moveSpeed = 50.0f;
+    bool mIsOrtho;// = false;
+    float mZoom;// = 1.0f;
+	float mMoveSpeed;// = 50.0f;
 
-	glm::mat4 translationMatrix;
-	glm::mat4 rotationMatrix;
-	glm::mat4 viewMatrix;
-	glm::vec3 viewVector;
-	glm::vec3 rotateXAxis;
-	glm::vec3 rotateYAxis;
-	glm::vec3 rotateZAxis;
-	glm::vec3 eyePosition;
-	glm::vec3 eyeLookPosition;
-
-	//----------------------------------
-	//glm::mat4 mTransform;
-	//glm::mat4 mXRotationMatrix;
-	//glm::mat4 mYRotationMatrix;
-	//glm::mat4 mZRotationMatrix;
-	//float mXAxisAngle;// degree
-	//float mYAxisAngle;// degree
-	//float mZAxisAngle;// degree
-	//tzVector3D mPosition;
-	//glm::vec3	mFace;
-	//glm::vec3	mUp;
-	//glm::vec3	mRight;
-	// mine
-	//tzMatrix	mTransform;
-	tzVector3D	mFace;
-	tzVector3D	mUp;
-	tzVector3D	mRight;
-	//tzMatrix mXRotationMatrix;
-	//tzMatrix mYRotationMatrix;
-	//tzMatrix mZRotationMatrix;
+	tzCoreCamera mCamera;
 	//----------------------------------
 
-    bool lmbDown = false;
-    bool midDown = false;
-	bool rmbDown = false;
-	glm::vec2 lmbDownCoord;
-	glm::vec2 midDownCoord;
-	glm::vec2 rmbDownCoord;
+    bool mLmbDown = false;
+    bool mMidDown = false;
+	bool mRmbDown = false;
+	tzPoint2D mLmbDownCoord;
+	tzPoint2D mMidDownCoord;
+	tzPoint2D mRmbDownCoord;
 	
-	int w_width;
-	int w_height;
-	float wheel_val;
+	int mWindowWidth;
+	int mWindowHeight;
+	float mWheelVal;
 };
 
 
